94c8564ec099ae2bc7b1de0b453b26662aa2d50d
Francesco Maestrelli <framae26@gmail.com>
2022-01-28 16:50:30 +0100

    pari&dispari

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,5 +1,34 @@
 // Palidroma 
+
 // Chiedere all’utente di inserire una parola 
 
-parseInt(prompt('Inserisci una parola'))
-// Creare una funzione per capire se la parola inserita è palindroma
+// let parola = parseInt(prompt('Inserisci una parola'))
+// let parolaDivisa;
+
+// // Creare una funzione per capire se la parola inserita è palindroma
+
+// function palindroma () {
+//     if (parolaDivisa = parola.split('')) {
+//         alert('la parola è palindroma')
+//     }
+// }
+
+let parola = parseInt(prompt('Inserisci una parola'));
+let parolaInversa = invertiParola(parola);
+
+if(parola == parolaInversa){
+    alert('la parola è palindroma');
+  } else {
+    alert('la parola non è palindroma');
+}
+
+function invertiParola(stringa){
+  let parolaInversa = parola;
+
+  for (let i = 0; i = stringa.length; i++) {
+    parolaInversa += stringa[i];
+    
+  }
+
+  return parolaInversa;
+}
